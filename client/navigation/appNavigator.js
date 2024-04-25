802a376d3b767c77bbfd3d2b642cb73e8b31d7e3
Muhammadali1029 <104990048+Muhammadali1029@users.noreply.github.com>
2024-04-25 16:02:42 +0100

    disaled header showing in homee screen

--- client/navigation/appNavigator.js
+++ client/navigation/appNavigator.js
@@ -2,8 +2,9 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../screens/HomeScreen';
-import ProductDetailScreen from '../screens/ProductDetailScreen';
+
+import HomeScreen from '../screens/homeScreen.js';
+import ProductDetailScreen from '../screens/productDetailScreen.js';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,7 +12,7 @@ function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
