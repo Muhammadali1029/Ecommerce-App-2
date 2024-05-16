c992a4073bc0a1a98ecb45ac1fadaed329370bc6
Muhammadali1029 <104990048+Muhammadali1029@users.noreply.github.com>
2024-05-16 20:22:02 +0100

    added app icon and update checking

--- client/App.js
+++ client/App.js
@@ -5,23 +5,23 @@ import * as Updates from 'expo-updates';
 import { useEffect } from 'react';
 
 export default function App() {
-  // async function onFetchUpdateAsync() {
-  //   try {
-  //     const update = await Updates.checkForUpdateAsync();
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
 
-  //     if (update.isAvailable) {
-  //       await Updates.fetchUpdateAsync();
-  //       await Updates.reloadAsync();
-  //     }
-  //   } catch (error) {
-  //     // You can also add an alert() to see the error message in case of an error when fetching updates.
-  //     alert(`Error fetching latest Expo update: ${error}`);
-  //   }
-  // }
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
 
-  // useEffect(() => {
-  //   onFetchUpdateAsync()
-  // }, [])
+  useEffect(() => {
+    onFetchUpdateAsync()
+  }, [])
 
   return <AppNavigator />;
 }
